47c359d52929b6118c149567b1ae3a857c44cadd
FelipeGitHUbMaster <felipe.pereira@edu.tbz.ch>
2025-06-05 15:44:29 +0200

    initial commit/ Project layout structure

---
+++ custom_mob_sounds/src/main/java/com/FelipeSpiderman/custommobsounds/CustomMobSounds.java
@@ -0,0 +1,10 @@
+package com.FelipeSpiderman.custommobsounds;
+
+import net.fabricmc.api.ModInitializer;
+
+public class CustomMobSounds implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        System.out.println("Custom Mob Sounds initialized!");
+    }
+}
